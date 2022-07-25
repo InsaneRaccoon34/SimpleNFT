0b01097f97354205b04c10f2bcbea9ae8431e73f
InsaneRaccoon34 <morozuk44@gmail.com>
2022-07-25 13:53:02 +0300

    init with working generator

---
+++ scripts/GenerateCharacter.py
@@ -0,0 +1,33 @@
+from email.mime import image
+from hashlib import new
+import os
+from random import randint, random
+from PIL import Image, ImageShow
+
+ASSETS_FOLDER = 'assets\\'
+NFT_FOLDER = "NFT_Collection"
+
+def generateCharacter():
+
+    layers = os.listdir(ASSETS_FOLDER)
+    seed = []
+
+    new_im = Image.new('RGB', (250, 250), (255,255,255))
+
+    for layer in layers:
+        lenght = len(os.listdir(ASSETS_FOLDER + layer))
+        randomNumber = randint(0, lenght - 1)
+        seed.append(randomNumber)
+
+        img = Image.open(ASSETS_FOLDER + layer + "\\" + layer + str(randomNumber) + '.png')
+        new_im.paste(img, (0,0), img.convert('RGBA'))
+
+    print(seed)
+    return new_im
+
+
+generateCharacter()
+
+for x in range(42):
+    pic = generateCharacter()
+    pic.save(NFT_FOLDER + "\\" + str(x) + '.png')
